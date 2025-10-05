1aaf193e6cd73ac5a0065355b6222940ecf5b3ff
Theerakarn <worknarair6@gmail.com>
2025-10-05 11:30:23 +0700

    Implement code changes to enhance functionality and improve performance

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -4,7 +4,7 @@ import * as schema from "~/database/schema";
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
